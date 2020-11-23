c1d5b643457a67b86a810cabeecb61c910e6f6a2
kdwVM <kdwkdw0078@gmail.com>
2020-11-23 20:52:06 +0900

    vm

---
+++ project2/testIO/testIO.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+int main(int argc, char** argv) {
+    const char* inputFilePath = argv[1];
+    char* outputFilePath = argv[2];
+
+    printf("input : %s\n output :%s\n", inputFilePath, outputFilePath);
+}
+
